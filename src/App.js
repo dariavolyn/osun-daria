ee91b2476a8433dd47bcdfd7aa04615b4b35637e
dariavolyn <75863158+dariavolyn@users.noreply.github.com>
2023-01-24 06:41:24 -0500

    Quote added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ function App() {
         <img className='intro-img' src={intro} alt='Get Engaged Conference participants group'></img>
         <h1 className='intro-title'>Get Engaged Conference</h1>
       </div>
-      
+
+      <div className='quote'>
+        <h2 className='quote-title'>NOT A TRADITIONAL ACADEMIC CONFERENCE</h2>
+        <p className='quote-text'>Growing a network of young social entrepreneurs and change agents who are actively using the liberal arts as a tool to develop solutions to challenges in their communities.</p>
+      </div>
+
     </div>
   );
 }
